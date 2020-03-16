6f6af98075ef87946b5e657d664a7bb66434f86f
barak <balmeshaan@gmail.com>
2020-03-16 17:01:18 +0300

    changed debug to false

--- untitled/settings.py
+++ untitled/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^cm5(vv4-r)vetdb)x1ha_vq=^e(4)jbv1xcu7o1y5jwy!=of0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["134.122.69.217", "127.0.0.1"]
 
