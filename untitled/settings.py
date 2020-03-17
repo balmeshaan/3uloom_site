6172376a53810d1afb9b1231bf1a1c93447a6467
barak <balmeshaan@gmail.com>
2020-03-17 16:31:02 +0300

    v2.3

--- untitled/settings.py
+++ untitled/settings.py
@@ -136,9 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
